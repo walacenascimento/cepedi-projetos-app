6541ce9745b98692e24520d550362f091824e404
Walace Nascimento <wlacens@hotmail.com>
2024-09-09 15:56:47 -0300

    Adicionando as Props no arquivo index.tsx do componente InputAddTask

--- to-do-list/src/components/InputAddTask/index.tsx
+++ to-do-list/src/components/InputAddTask/index.tsx
@@ -1,15 +1,23 @@
 import {Feather} from '@expo/vector-icons';
-import {InputContainer, Input, InputButton } from './styles' 
+import {InputContainer, Input, InputButton } from './styles';
 
-export function InputAddTask(){
+type Props = {
+    onPress: () => void;
+    onChangeText: (texto: string) => void;
+    value: string;
+}
+
+export function InputAddTask({onPress, onChangeText, value} : Props){
     return(
         <InputContainer>
             <Input
                 placeholder='Digite a tarefa'
                 placeholderTextColor="white"
                 keyboardType='default'
+                value={value}
+                onChangeText={onChangeText}
             />
-            <InputButton>
+            <InputButton onPress={onPress}>
                 <Feather name="plus-square" size={24} color="white" />
             </InputButton>
         </InputContainer>
